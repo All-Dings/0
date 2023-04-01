a200be317c4151a946dbc8ba67cf2a2f03dca369
Michael Holzheu <michael@holzheu.de>
2023-04-02 01:36:14 +0200

    Add First Script

--- 9000038.bash
+++ 9000038.bash
@@ -1,3 +1,7 @@
 # Du-Script
 
-find . -type file | grep -E './[a-zA-Z0-9]*\.(md|jpg|markdown|bash|bashrc)' | (while read -r FileName; do FileSize=$(stat -f "%z" $FileName); SumSize=$((SumSize + FileSize)); printf "%-16s %8s %5s\n" $FileName $FileSize $SumSize; done)
+# First Script
+ls -l | tail -n +2 | (while read -r Line; do FileSize=$(echo $Line | awk '{print $5}'); FileName=$(echo $Line | awk '{print $9}'); SumSize=$((SumSize + FileSize)); printf "%-16s %8s %5s\n" $FileName $FileSize $SumSize; done) | tail -n 1
+
+# Second Script
+find . -type file | grep -E './[a-zA-Z0-9]*\.(md|jpg|markdown|bash|bashrc)' | (while read -r FileName; do FileSize=$(stat -f "%z" $FileName); SumSize=$((SumSize + FileSize)); printf "%-16s %8s %5s\n" $FileName $FileSize $SumSize; done) | tail -n 1
