501a717711617e3c258a403ce12e818a19e9c324
Michael Holzheu <michael@holzheu.de>
2023-04-30 04:21:59 +0200

    Add Reference-Support

--- 300010010.py
+++ 300010010.py
@@ -6,8 +6,9 @@ The Dings-Lib-Python is a [Dings-Lib](300010000.md) in [Python](9010003.md).
 
 ## Imports
 
-import re
 import os
+import re
+import sys
 
 ## Directory containing the Markdown files
 Dings_Directory = os.getcwd()
@@ -16,8 +17,7 @@ Dings_Directory = os.getcwd()
 
 ### Dings-Regular-Expressions
 Reg_Exp_Number_File = '\d+' + '.' + '\w+'
-Reg_Exp_Name_Exact = '(' + '\"|=|\?|\!|\:|\(|\)|#|-|\w|\s|@' + ')' + '+'
-Reg_Exp_Name = '(' + '.' + ')' + '+'
+Reg_Exp_Name = '(?:' + '\"|=|\?|\!|\:|\(|\)|#|-|\w|\s|@' + ')' + '+'
 
 ### Apply [Reg_Exp](9000103.md) to [Line](700011.md) and print the [Match](404.md)
 def Reg_Exp_Test(Reg_Exp, Line):
@@ -49,12 +49,19 @@ def Reference_Reg_Exp_Test():
 	Reg_Exp_Test(Reference_Reg_Exp, 'The Man [Michael-Holzheu](0.md) creates the Dings-Project.')
 	Reg_Exp_Test(Reference_Reg_Exp, '[Michael-Holzheu](0.md) builds Dings-Project.')
 	Reg_Exp_Test(Reference_Reg_Exp, 'The Digns-Prject is created by [Michael-Holzheu](0.md)')
+	Reg_Exp_Test(Reference_Reg_Exp, 'Height is a [Dimension-Interval](10000021.md) for the [Altitude-Dimension](10000030.md).')
 
 ### Get Name from Reference
-Name_from_Reference_Reg_Exp = re.compile('\[' + '(' + Reg_Exp_Name + ')' + '\]' + '\(' + Reg_Exp_Number_File + '\)')
+Name_From_Reference_Reg_Exp = re.compile('\[' + '(' + Reg_Exp_Name + ')' + '\]' + '\(' + Reg_Exp_Number_File + '\)')
 
-def Name_from_Reference_Reg_Exp_Test():
-	Reg_Exp_Test(Name_from_Reference_Reg_Exp, '[Michael-Holzheu](0.md)')
+def Name_From_Reference_Reg_Exp_Test():
+	Reg_Exp_Test(Name_From_Reference_Reg_Exp, '[Michael-Holzheu](0.md)')
+
+### Get Number from Reference
+Number_From_Reference_Reg_Exp = re.compile('\[' + Reg_Exp_Name + '\]' + '\(' + '(' + '\d+' + ')' + '.' + '\w+' + '\)')
+
+def Number_From_Reference_Reg_Exp_Test():
+	Reg_Exp_Test(Number_From_Reference_Reg_Exp, '[Michael-Holzheu](0.md)')
 
 ## Number-File-List
 Number_File_List = {}
@@ -75,6 +82,9 @@ def Print_Number_File(Number_File):
 	Name = Number_File['Name']
 	Number = Number_File['Number']
 	print(f'{Number} "{Name}"')
+	for Reference in Number_File['Target_References']:
+		Source = Reference['Source']
+		print(f"  - {Reference['Name']} [{Source['Name']}]({Source['Number']})")
 
 ## Read Data of a Number-File
 def Read_Number_File(File_Name):
@@ -87,7 +97,37 @@ def Read_Number_File(File_Name):
 	Number_File = {}
 	Number_File['Number'] = int(Number)
 	Number_File['Name'] = Name.strip()
+	Number_File['Source_References'] = []
+	Number_File['Target_References'] = []
 	return Number_File
+
+## Read References of a Number-File
+def Read_Number_File_References(File_Name):
+	Source_Number = Number_Reg_Exp.match(File_Name).group(1)
+	Source = Number_File_List[int(Source_Number)]
+	with open(os.path.join(Dings_Directory, File_Name), 'r') as File:
+		Lines = File.readlines()
+	for Line in Lines:
+		Match = Reference_Reg_Exp.search(Line)
+		if not Match:
+			continue
+		Reference = Match.group(1)
+		Target_Number = Number_From_Reference_Reg_Exp.search(Reference).group(1)
+		Target_Name = Name_From_Reference_Reg_Exp.search(Reference).group(1)
+
+		if not int(Source_Number) in Number_File_List:
+			print(f"Source-Number {Source_Number} not found.", file=sys.stderr)
+			continue
+		if not int(Target_Number) in Number_File_List:
+			print(f"Target-Number {Target_Number} not found.", file=sys.stderr)
+			continue
+		Target = Number_File_List[int(Target_Number)]
+		Reference = {}
+		Reference['Source'] = Source
+		Reference['Target'] = Target
+		Reference['Name'] = Target_Name
+		Source['Source_References'].append(Reference)
+		Target['Target_References'].append(Reference)
 
 ## Read all Number-Files into a [Linked-List](250000019.md)
 def Read_Number_File_List():
@@ -98,6 +138,10 @@ def Read_Number_File_List():
 		Number_File = Read_Number_File(File_Name)
 		### Append Number-File-Dictionary to Number-File-List
 		Number_File_List[Number_File['Number']] = Number_File
+	for File_Name in os.listdir(Dings_Directory):
+		if not Number_Reg_Exp.search(File_Name):
+			continue
+		Read_Number_File_References(File_Name)
 
 ## Print all Number-Files
 def Print_Number_File_List():
@@ -112,8 +156,11 @@ def Number_File_List_Test():
 	Read_Number_File_List()
 	Print_Number_File_List()
 
+Number_From_Reference_Reg_Exp_Test()
+Name_From_Reference_Reg_Exp_Test()
 Number_Reg_Exp_Test()
 Name_Reg_Exp_Test()
 Reference_Reg_Exp_Test()
-Name_from_Reference_Reg_Exp_Test()
-Number_File_List_Test()
+Name_From_Reference_Reg_Exp_Test()
+Read_Number_File_List()
+Print_Number_File_List()
--- 300010011.pl
+++ 300010011.pl
@@ -8,8 +8,12 @@ The Dings-Lib-Perl is a [Dings-Lib](300010000.md) in [Perl](9010001.md).
 
 use 5.010;
 use strict;
+use utf8;
 use warnings;
 use Cwd;
+
+binmode(STDOUT, "encoding(UTF-8)");
+use open ':std', ':encoding(UTF-8)';
 
 ## Directory containing the Markdown files
 my $Dings_Directory = getcwd;
@@ -17,8 +21,7 @@ my $Dings_Directory = getcwd;
 ## Regular-Expressions
 
 my $Reg_Exp_Number_File = '\d+' . '.' . '\w+';
-my $Reg_Exp_Name_Exact = '(' . '\"|=|\?|\!|\:|\(|\)|#|-|\w|\s|@' . ')' . '+';
-my $Reg_Exp_Name = '(' . '.' . ')' . '+';
+my $Reg_Exp_Name = '[' . '\"\=\?\!\:\(\)\#\-\w\s@' . ']' . '+';
 
 ### Apply [Reg_Exp](9000103.md) to [Line](700011.md) and print the [Match](404.md)
 sub Reg_Exp_Test($$)
@@ -32,7 +35,7 @@ sub Reg_Exp_Test($$)
 	printf("\"%s\" -> \"%s\"\n", $Line, $Match);
 }
 
-### Get Dings-Name from First-Line
+### Get Dings-Name From First-Line
 my $Name_Reg_Exp = '^' . '#' . ' ' . '(' . $Reg_Exp_Name . ')' . '\s*' . '$';
 
 sub Name_Reg_Exp_Test()
@@ -45,7 +48,7 @@ sub Name_Reg_Exp_Test()
 	Reg_Exp_Test($Name_Reg_Exp, '# Brașov');
 }
 
-### Get Number from Number-File
+### Get Number From Number-File
 my $Number_Reg_Exp = '^' . '(' . '\d+' . ')' . '.md' . '$';
 
 sub Number_Reg_Exp_Test()
@@ -54,7 +57,7 @@ sub Number_Reg_Exp_Test()
 	Reg_Exp_Test($Number_Reg_Exp, '341324.md');
 }
 
-### Get Reference from Line
+### Get Reference From Line
 my $Reference_Reg_Exp = '(' . '\[' . $Reg_Exp_Name . '\]' . '\(' . $Reg_Exp_Number_File . '\)' . ')';
 
 sub Reference_Reg_Exp_Test()
@@ -62,18 +65,28 @@ sub Reference_Reg_Exp_Test()
 	Reg_Exp_Test($Reference_Reg_Exp, 'The Man [Michael-Holzheu](0.md) creates the Dings-Project.');
 	Reg_Exp_Test($Reference_Reg_Exp, '[Michael-Holzheu](0.md) builds Dings-Project.');
 	Reg_Exp_Test($Reference_Reg_Exp, 'The Digns-Prject is created by [Michael-Holzheu](0.md)');
+	Reg_Exp_Test($Reference_Reg_Exp, 'Height is a [Dimension-Interval](10000021.md) for the [Altitude-Dimension](10000030.md).');
 }
 
-### Get Name from Reference
-my $Name_from_Reference_Reg_Exp = '\[' . '(' . $Reg_Exp_Name . ')' . '\]' . '\(' . $Reg_Exp_Number_File . '\)';
+### Get Name From Reference
+my $Name_From_Reference_Reg_Exp = '\[' . '(' . $Reg_Exp_Name . ')' . '\]' . '\(' . $Reg_Exp_Number_File . '\)';
 
-sub Name_from_Reference_Reg_Exp_Test()
+sub Name_From_Reference_Reg_Exp_Test()
 {
-        Reg_Exp_Test($Name_from_Reference_Reg_Exp, '[Michael-Holzheu](0.md)')
+        Reg_Exp_Test($Name_From_Reference_Reg_Exp, '[Michael-Holzheu](0.md)');
+}
+
+### Get Number from Reference
+my $Number_From_Reference_Reg_Exp = '\[' . $Reg_Exp_Name . '\]' . '\(' . '(' . '\d+' . ')' . '.' . '\w+' . '\)';
+# my $Number_From_Reference_Reg_Exp = '\[.*\]' . '\(' . '(' . '\d+' . ')' . '.' . '\w+' . '\)';
+
+sub Number_From_Reference_Reg_Exp_Test()
+{
+	Reg_Exp_Test($Number_From_Reference_Reg_Exp, '[Michael-Holzheu](0.md)');
 }
 
 ## Number-File-List
-my %Number_File_List = ();
+my $Number_File_List = {};
 
 ## Print Number-File
 sub Print_Number_File($)
@@ -82,9 +95,14 @@ sub Print_Number_File($)
 	my $Name = $Number_File->{'Name'};
 	my $Number = $Number_File->{'Number'};
 	printf("%d \"%s\"\n", $Number, $Name);
+	my $h = $Number_File->{'Target_References'};
+	foreach my $Reference (sort {$a->{'Id'} <=> $b->{'Id'}} values %$h) {
+		my $Source = $Reference->{'Source'};
+		printf("  - $Reference->{'Name'} [$Source->{'Name'}]($Source->{'Number'})\n");
+	}
 }
 
-## Remove White-Space from the Start and End of a String
+## Remove White-Space From the Start and End of a String
 sub Strip($)
 {
 	my $String = $_[0];
@@ -113,8 +131,55 @@ sub Read_Number_File($)
 	$Number_File = {};
 	$Number_File->{'Number'} = $Number;
 	$Number_File->{'Name'} = $Name;
+	$Number_File->{'Source_References'} = {};
+	$Number_File->{'Target_References'} = {};
 
 	return $Number_File;
+}
+
+# Required for List-Simulation ;-)
+my $Reference_Id = 0;
+
+## Read References of a Number-File
+sub Read_Number_File_References($)
+{
+	my ($Line, $Reference, $Target_Number, $Target_Name, $Target);
+	my $File_Name = $_[0];
+
+	$File_Name =~ /$Number_Reg_Exp/;
+	my $Source_Number = $1;
+	my $Source = $Number_File_List->{$Source_Number};
+
+	open my $File, '<', $File_Name;
+	while ($Line = <$File>)  {
+		if ($Line !~ /$Reference_Reg_Exp/) {
+			next;
+		}
+		$Reference = $1;
+		$Reference =~ /$Number_From_Reference_Reg_Exp/;
+		$Target_Number = $1;
+		$Reference =~ /$Name_From_Reference_Reg_Exp/;
+		$Target_Name = $1;
+
+		if (!exists($Number_File_List->{$Source_Number})) {
+			print STDERR "Source-Number $Source_Number not found.\n";
+			next;
+		}
+		if (!exists($Number_File_List->{$Target_Number})) {
+			print STDERR "Target-Number $Target_Number not found.\n";
+			next;
+		}
+		$Target = $Number_File_List->{$Target_Number};
+		$Reference = {};
+		$Reference->{'Source'} = $Source;
+		$Reference->{'Target'} = $Target;
+		$Reference->{'Name'} = $Target_Name;
+		$Reference->{'Id'} = $Reference_Id;
+		$Source->{'Source_References'}{$Reference_Id} = $Reference;
+		$Target->{'Target_References'}{$Reference_Id} = $Reference;;
+		$Reference_Id++;
+	}
+	close $File;
 }
 
 ## Read all Number-Files into a [Linked-List](250000019.md)
@@ -134,7 +199,13 @@ sub Read_Number_File_List()
 		}
 		$Number_File = Read_Number_File($File_Name);
 		### Append Number-File-Dictionary to Number-File-List
-		$Number_File_List{$Number_File->{'Number'}} = Read_Number_File($File_Name);
+		$Number_File_List->{$Number_File->{'Number'}} = Read_Number_File($File_Name);
+	}
+	foreach my $File_Name (@File_List) {
+		if ($File_Name !~ /$Number_Reg_Exp/) {
+			next;
+		}
+		Read_Number_File_References($File_Name)
 	}
 }
 
@@ -143,7 +214,7 @@ sub Print_Number_File_List()
 {
 	my $Number_File;
 
-	foreach my $Number_File (sort {$a->{'Number'} <=> $b->{'Number'}} values %Number_File_List) {
+	foreach my $Number_File (sort {$a->{'Number'} <=> $b->{'Number'}} values %$Number_File_List) {
 		Print_Number_File($Number_File);
 	}
 }
@@ -155,8 +226,11 @@ sub Number_File_List_Test()
 	Print_Number_File_List();
 }
 
+Number_From_Reference_Reg_Exp_Test();
+Name_From_Reference_Reg_Exp_Test();
 Number_Reg_Exp_Test();
 Name_Reg_Exp_Test();
 Reference_Reg_Exp_Test();
-Name_from_Reference_Reg_Exp_Test();
-Number_File_List_Test();
+Name_From_Reference_Reg_Exp_Test();
+Read_Number_File_List();
+Print_Number_File_List();
