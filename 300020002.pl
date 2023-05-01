ba8844b975a459cef9a90751a762d56faf6180b9
Michael Holzheu <michael@holzheu.de>
2023-05-02 00:27:25 +0200

    Add Dings-Tool-Perl

---
+++ 300020002.pl
@@ -0,0 +1,10 @@
+# Dings-Tool-Perl
+=for comment
+
+The Dings-Tool-Perl is the [Dings-Tool](300020000.md) written in the [Perl-Programming-Language](9010001.md).
+=cut
+
+BEGIN {push @INC, '.'}
+use Dings;
+
+Dings::File_Extension_Reg_Exp_Test();
