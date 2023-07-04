e4b5c00bb3a6542e803c946d7fe68d891c9fa7fb
Michael Holzheu <michael@holzheu.de>
2023-07-05 00:15:42 +0200

    Add --mathml Pandoc Option

--- 300020001.py
+++ 300020001.py
@@ -548,7 +548,7 @@ class Dings_Html_Generate_Command_Class(Dings_Html_Command_Class):
 			with Context_Lib.redirect_stdout(File):
 				Self.Gen_Inline_Ids_And_Objects(Markdown_File)
 		# Os.system(f'pandoc --section-divs -f markdown-auto_identifiers --metadata title="{Title}" --standalone --template {Htm_Pandoc_File_Name} {Md_Pandoc_File_Name} -o {Output_File_Name}')
-		Os.system(f'pandoc -f markdown-auto_identifiers --metadata title="{Title}" --standalone --template {Htm_Pandoc_File_Name} {Md_Pandoc_File_Name} -o {Output_File_Name}')
+		Os.system(f'pandoc -f markdown-auto_identifiers --metadata title="{Title}" --standalone --mathml --template {Htm_Pandoc_File_Name} {Md_Pandoc_File_Name} -o {Output_File_Name}')
 		Os.unlink(Htm_Pandoc_File_Name)
 		Os.unlink(Md_Pandoc_File_Name)
 		return 0
