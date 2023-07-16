9a73d2cec35ada20c7a41c4ffce6084e4370f64b
Michael Holzheu <michael@holzheu.de>
2023-07-16 21:01:23 +0200

    Remove Id=

--- 300040004.py
+++ 300040004.py
@@ -5,7 +5,7 @@ class Dings_Video_Class(Dings_Lib.Object_Class):
 		super().__init__(Caption, Json_Path, Parameter, Tag, Anchor)
 
 	def Generate_Html(Self, Html_Id):
-		print(f'<video id="{Self.Description}" width="100%" height="auto" controls>')
+		print(f'<video width="100%" height="auto" controls>')
 		if Self.Extension == "mp4":
 			print(f'  <source src="{Self.File_Path}" type=video/mp4>')
 		else:
