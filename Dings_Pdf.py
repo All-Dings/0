79a0512482195cc3efc59c93f5441bb1df438e52
Michael Holzheu <michael@holzheu.de>
2023-07-16 21:04:22 +0200

    Convert Files to Soft-Links

--- Dings_Pdf.py
+++ Dings_Pdf.py
@@ -1,13 +1,1 @@
-import Dings_Lib
-
-class Dings_Pdf_Class(Dings_Lib.Object_Class):
-	def __init__(Self, Caption, Json_Path, Parameter, Tag, Anchor):
-		super().__init__(Caption, Json_Path, Parameter, Tag, Anchor)
-
-	def Generate_Html(Self, Html_Id):
-		print(f'<a href="{Self.Number}.pdf" type="application/pdf" target="_blank">')
-		print(f' <div class="Overlay-Image-Container">')
-		print(f'  <img src="{Self.Number}.jpg" class="Overlay-Image-Parent" alt="{Self.Caption}" width="100%"/>')
-		print(f'  <img src="300000101.png" class="Overlay-Image-Child" width="100%"/>')
-		print(f' </div>')
-		print(f'</a>')
+300040005.py
